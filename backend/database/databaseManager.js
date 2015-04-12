1460ebe6451695480b97d5e525c817bb9152435b
jtcotton63 <jtcotton63@gmail.com>
2015-04-11 23:53:27 -0600

    Working on searching by tags problem

--- backend/database/databaseManager.js
+++ backend/database/databaseManager.js
@@ -17,7 +17,19 @@ var MONGO_DB_ADDR = 'mongodb://52.11.71.104';
 var MONGO_DB_NAME = "cs360fse";
 var MONGO_COLLECTION_NAME = "fooditems";
 
-// Food Item Schema definition
+// Schemas
+// var foodTagSchema = new mongoose.Schema( {
+// 	food_name: { type: String },
+// 	food_description: { type: String },
+// 	restaurant: { type: String },
+// 	restaurant_location: { type: String },
+// 	price: { type: Number, default: 0 },
+// 	calories: { type: Number, default: 0 },
+// 	food_type: { type: String },
+// 	food_tags:[ { type: String, ref: 'food_tag' } ],
+// 	image: { type: String }
+// } );
+
 var foodItemSchema = new mongoose.Schema( {
 	food_name: { type: String },
 	food_description: { type: String },
